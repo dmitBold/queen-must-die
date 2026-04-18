fac53c6d50bd891c82f97e7d0f9035c162605736
DIMASHARDBASS111 <ikgnnn000@gmail.com>
2026-04-18 21:41:13 +0300

    Pickup sound added

--- Assets/Scripts/Core/AudioService.cs
+++ Assets/Scripts/Core/AudioService.cs
@@ -5,6 +5,7 @@ namespace Core
     public class AudioService : MonoBehaviour
     {
         [SerializeField] private AudioSource sfxSource;
+        public static AudioClip pickup_Sound;
         public AudioSource musicSource;
 
         public void PlaySound(AudioClip clip, float volume = 1f)
@@ -12,6 +13,13 @@ namespace Core
             if (clip == null) return;
             sfxSource.PlayOneShot(clip, volume);
         }
+
+        /*public void PlayPickUP(float volume = 1f)
+        {
+            if(pickup_Sound == null) return;
+            sfxSource.PlayOneShot(pickup_Sound, volume);
+            Debug.Log("VGVYVGYVYGVYGVYVG");
+        }*/
 
         public void PlaySoundAtPoint(AudioClip clip, Vector3 position, float volume = 1f)
         {
