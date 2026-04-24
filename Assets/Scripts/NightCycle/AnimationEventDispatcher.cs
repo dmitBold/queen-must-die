0a65ca38674732567c105403c9edb15c89b2933a
DIMASHARDBASS111 <ikgnnn000@gmail.com>
2026-04-24 23:08:38 +0300

    daycicle transition to docs added

--- Assets/Scripts/NightCycle/AnimationEventDispatcher.cs
+++ Assets/Scripts/NightCycle/AnimationEventDispatcher.cs
@@ -7,9 +7,29 @@ namespace NightCycle
     {
         public UnityEvent onAnimationTrigger;
 
+        public UnityEvent onAnimationTrigger_additional;
+
+        public bool can_trigger = false;
+
         public void TriggerEvent()
         {
-            onAnimationTrigger?.Invoke();
+            if (can_trigger)
+            {
+                onAnimationTrigger?.Invoke();
+            }
+        }
+
+        public void TriggerEvent_additional()
+        {
+            if (can_trigger)
+            {
+                onAnimationTrigger_additional?.Invoke();
+            }
+        }
+
+        public void set_trigger_condition(bool condition)
+        {
+            can_trigger = condition;
         }
     }
 }
